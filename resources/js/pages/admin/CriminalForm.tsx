a267d46d5efe2207664cd8ecdffc4bb0322a526c
Daniel Assani <84669598+asddaniel@users.noreply.github.com>
2025-07-01 11:28:09 +0200

    add build

--- resources/js/pages/admin/CriminalForm.tsx
+++ resources/js/pages/admin/CriminalForm.tsx
@@ -12,7 +12,7 @@ import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from '@
 
 // Helper pour l'URL de base de l'API (à mettre dans un fichier .env.local)
 // VITE_API_BASE_URL=http://localhost:8000
-const API_BASE_URL = (import.meta as any).env.VITE_API_BASE_URL || 'http://localhost:8000';
+const API_BASE_URL = location.origin+""  // (import.meta as any).env.VITE_API_BASE_URL || 'http://localhost:8000';
 
 interface ExistingPhoto {
     id: number;
